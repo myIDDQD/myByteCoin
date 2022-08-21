4eeb9a69f8ba5421ec0f442686b02d6df7a3eb1a
myIDDQD <109028077+myIDDQD@users.noreply.github.com>
2022-08-21 12:22:56 +0300

    Add files via upload

---
+++ myByteCoin/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  myByteCoin
+//
+//  Created by Mykola on 21.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
